d407411866c513e106b3e67bc7a312338ae38047
Trinity <trinitycoyle@yahoo.com>
2017-12-16 10:00:31 -0500

    first deploy

--- server.js
+++ server.js
@@ -21,4 +21,6 @@ var forceSSL = function() {
   }
 }
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
