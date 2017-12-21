24a753b6890a4ab57f4bf08836b9cb6975adf37c
Trinity <trinitycoyle@yahoo.com>
2017-12-21 18:04:48 -0500

    update

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-var MongoClient = require('mongodb').MongoClient;
+/*var MongoClient = require('mongodb').MongoClient;
 
 var uri = "mongodb://TrinityCoyle:1999Zirconia!@cluster0-shard-00-00-sx3i1.mongodb.net:27017,cluster0-shard-00-01-sx3i1.mongodb.net:27017,cluster0-shard-00-02-sx3i1.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
 MongoClient.connect(uri, function(err, db) {
   db.close();
-});
+});*/
 
 //app.use(forceSSL());
 
